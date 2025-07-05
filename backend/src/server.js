c5e544e09ab9cc71f45a9068b5bce55930806264
erhanikinci <erhanikinci61@gmail.com>
2025-07-06 00:32:39 +0300

    comment routes added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,27 +1,27 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
 
-
-import userRoutes from "./routes/user.route.js"
-import postRoutes from "./routes/post.route.js"
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
 
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 
 const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use(clerkMiddleware())
-
+app.use(clerkMiddleware());
 
 connectDB();
 app.get("/", (req, res) => res.send("Hello from server"));
 
-app.use("/api/users", userRoutes)
-app.use("/api/posts", postRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
 
 // error handling middleware
 app.use((err, req, res, next) => {
@@ -35,7 +35,9 @@ const startServer = async () => {
 
     // listen for local development
     if (ENV.NODE_ENV !== "production") {
-      app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
+      app.listen(ENV.PORT, () =>
+        console.log("Server is up and running on PORT:", ENV.PORT)
+      );
     }
   } catch (error) {
     console.error("Failed to start server:", error.message);
