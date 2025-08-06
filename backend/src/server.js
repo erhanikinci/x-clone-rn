d360d444112a2895f8d46f80f3d085d496252c43
erhanikinci <erhanikinci61@gmail.com>
2025-08-06 20:26:31 +0300

    vercel config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,9 +5,11 @@ import { clerkMiddleware } from "@clerk/express";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import { arcjetMiddleware } from "./middleware/archjet.middleware.js";
 
 const app = express();
 
@@ -15,13 +17,14 @@ app.use(cors());
 app.use(express.json());
 
 app.use(clerkMiddleware());
+app.use(arcjetMiddleware);
 
-connectDB();
 app.get("/", (req, res) => res.send("Hello from server"));
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 // error handling middleware
 app.use((err, req, res, next) => {
@@ -35,9 +38,7 @@ const startServer = async () => {
 
     // listen for local development
     if (ENV.NODE_ENV !== "production") {
-      app.listen(ENV.PORT, () =>
-        console.log("Server is up and running on PORT:", ENV.PORT)
-      );
+      app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
     }
   } catch (error) {
     console.error("Failed to start server:", error.message);
@@ -46,3 +47,6 @@ const startServer = async () => {
 };
 
 startServer();
+
+// export for vercel
+export default app;
